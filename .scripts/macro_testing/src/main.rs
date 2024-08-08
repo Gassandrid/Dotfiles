179a53beb94e05fa313e9d94c4766312d484d05f
ewanpedersenUVM <ejpeders@uvm.edu>
2024-08-08 13:50:28 -0700

    more stuff ig

--- .scripts/macro_testing/src/main.rs
+++ .scripts/macro_testing/src/main.rs
@@ -2,6 +2,11 @@ use rdev::{listen, Event};
 use rdev::{simulate, Button, EventType, Key, SimulateError};
 use std::{thread, time};
 
+// file writing
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 fn send(event_type: &EventType) {
     let delay = time::Duration::from_millis(20);
     match simulate(event_type) {
@@ -65,11 +70,60 @@ fn write(s: &str) {
 }
 
 fn callback(event: Event) {
-    println!("My callback {:?}", event);
     if let Some(string) = event.name {
-        println!("User wrote {:?}", string);
+        // make sure input is a key and not a mouse movement, and extract the char
+        if string.len() == 1 {
+            let mut fileLocal = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open("keylog.txt")
+                .unwrap();
+            // make sure it is a key and not a mouse movement, and that it is only one char
+            fileLocal.write_all(string.as_bytes()).unwrap();
+        }
+
+        // if user starts to type "execute order 66" then execute a function called order_66
+        // get the last 16 characters from the file
+        let buffer = std::fs::read_to_string("keylog.txt").unwrap();
+        let buffer_len = buffer.len();
+        if buffer_len >= 16 {
+            let last_16 = &buffer[buffer_len - 16..buffer_len];
+            if last_16 == "execute order 66" {
+                order_66();
+            }
+        }
     }
 }
+
+fn order_66() {
+    println!("Order 66 executed");
+}
+
+// struct keylog {
+//     file: File,
+// }
+
+// impl keylog {
+//     fn new() -> keylog {
+//         keylog {
+//             file: File::create("keylog.txt").unwrap(),
+//         }
+//     }
+//     fn append(&mut self, s: &str) {
+//         // appent all the keys to a file
+//         self.file.write_all(s.as_bytes()).unwrap();
+//     }
+//     fn callback(&mut self, event: Event) {
+//         println!("My callback {:?}", event);
+//         if let Some(string) = event.name {
+//             // make sure input is a key and not a mouse movement, and extract the char
+//             if string.len() == 1 {
+//                 self.append(&string);
+//                 println!("{}", string);
+//             }
+//         }
+//     }
+// }
 
 fn main() {
     // collect args
@@ -80,6 +134,22 @@ fn main() {
     //         send(&EventType::KeyPress(Key::Space));
     //         send(&EventType::KeyRelease(Key::Space));
     //     }
+
+    // if keylog.txt already exists, delete it and create a new one
+    let _ = std::fs::remove_file("keylog.txt");
+    let _ = File::create("keylog.txt");
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("keylog.txt")
+        .unwrap();
+
+    let string = "hello world";
+
+    for c in string.chars() {
+        file.write_all(c.to_string().as_bytes()).unwrap();
+    }
 
     // This will block.
     if let Err(error) = listen(callback) {
