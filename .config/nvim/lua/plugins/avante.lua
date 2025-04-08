7c2c93a04e76f4e511b9a3f1525b1522b4ccdbb5
Gassandrid <pedersenewan@gmail.com>
2025-04-08 08:39:56 -0400

    new avante stuff

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,14 +1,16 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  lazy = false,
-  version = "*", -- set this if you want to always pull the latest change
+  version = false, -- Never set this value to "*"! Never!
   opts = {
     default = {},
     provider = "copilot", -- Recommend using Claude
     auto_suggestions_provider = "copilot",
     copilot = {
       model = "claude-3.7-sonnet",
+    },
+    web_search_engine = {
+      provider = "tavily", -- tavily, serpapi, searchapi, google or kagi
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -20,6 +22,10 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
